962811ad4b7740f54b75625a8b7c4cb109318603
EffexDev <effexdev@gmail.com>
2025-02-14 23:23:52 -0500

    Styled the plan card, just need images. Eventually will pull the plans from DB

--- src/components/PlansCard.tsx
+++ src/components/PlansCard.tsx
@@ -1,15 +1,39 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import FadeIn from '../functions/FadeIn';
 
 
-function PlansCard({ title, description, price }: { title: string; description: string, price: number }) {
+function PlansCard({ title, description, price, img, alt, button, site }: { title: string; description: string, price: number, img: string, alt: string, button: string, site: string }) {
     return (
-        <Card>
-            <CardContent>{title}</CardContent>
-            <CardContent>{description}</CardContent>
-            <CardContent>${price}</CardContent>
+        <FadeIn>
+        <Card sx={{borderRadius: 5, padding: 4}}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={img}
+            title={alt}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body1" gutterBottom sx={{ color: 'text.secondary', whiteSpace: "pre-line" }}>
+              {description}
+            </Typography>
+            <Typography variant='h5' sx={{paddingTop: 3}}>
+                ${price} + {site}
+            </Typography>
+          </CardContent>
+          <CardActions>        
+            <Button variant="contained" size="small">{button}</Button>
+          </CardActions>
         </Card>
-    )
-}
+        </FadeIn>
+      );
+    }
 
 export default PlansCard;
+
