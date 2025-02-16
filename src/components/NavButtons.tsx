efdc30997093588fe2b2fa3f7bca5b395a36cb63
EffexDev <effexdev@gmail.com>
2025-02-16 11:08:37 +0800

    Spicy commit to master

--- src/components/NavButtons.tsx
+++ src/components/NavButtons.tsx
@@ -2,25 +2,13 @@ import { Link, useLocation } from "react-router";
 
 function NavButtons({ flexDirection = "row" }) {
   const location = useLocation();
-  const isAboutPage = location.pathname === "/about";
+  const pathname = location.pathname;
 
   return (
     <nav className={`flex flex-${flexDirection} items-center justify-between w-full sm:w-auto`}>
-      {isAboutPage ? (
-        <>
-        <Link to="/" className="px-4 py-5 sm:px-6">Home</Link>
-        <p className="px-4 py-5 sm:px-6">Services</p>
-        <p className="px-4 py-5 sm:px-6">Projects</p>
-        </>
-
-      ) : (
-        
-        <>
-          <Link to="about" className="px-4 py-5 sm:px-6">About</Link>
-          <p className="px-4 py-5 sm:px-6">Services</p>
-          <p className="px-4 py-5 sm:px-6">Projects</p>
-        </>
-      )}
+      {pathname !== "/" && <Link to="/" className="px-4 py-5 sm:px-6">Home</Link>}
+      {pathname !== "/about" && <Link to="/about" className="px-4 py-5 sm:px-6">About</Link>}
+      {pathname !== "/projects" && <Link to="/projects" className="px-4 py-5 sm:px-6">Projects</Link>}
     </nav>
   );
 }
