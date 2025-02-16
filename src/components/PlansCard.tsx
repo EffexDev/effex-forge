0e4ead900018c51841641f461b9082c35ca03b95
EffexDev <effexdev@gmail.com>
2025-02-16 16:27:26 +0800

    Minor changes to spacing. Finished contact page

--- src/components/PlansCard.tsx
+++ src/components/PlansCard.tsx
@@ -10,25 +10,25 @@ import FadeIn from '../functions/FadeIn';
 function PlansCard({ title, description, price, img, alt, button, site }: { title: string; description: string, price: number, img: string, alt: string, button: string, site: string }) {
     return (
         <FadeIn>
-        <Card className="h-full flex flex-col" sx={{borderRadius: 5, padding: 2}}>
+        <Card className="h-full flex flex-col" sx={{maxWidth: "100%", padding: 2}}>
           <CardMedia
-            sx={{ height: 200, borderRadius: 5 }}
+            sx={{ height: 200, borderRadius: 1}}
             image={img}
             title={alt}
           />
           <CardContent>
-            <Typography gutterBottom variant="h4" component="div">
+            <Typography gutterBottom variant="h5" component="div" sx={{textAlign: "center"}}>
               {title}
             </Typography>
-            <Typography variant="body1" gutterBottom sx={{ color: 'text.secondary', whiteSpace: "pre-line" }}>
+            <Typography variant="body1" gutterBottom sx={{ color: 'text.secondary', whiteSpace: "pre-line", textAlign: "center"}}>
               {description}
             </Typography>
           </CardContent>
           <CardActions className="flex flex-col" sx={{marginTop: "auto"}}>    
-            <Typography variant='h5'> 
+            <Typography variant='h6' sx={{textAlign: "center"}}> 
                 ${price}/mo + {site}
             </Typography>    
-            <Button variant="contained" size="large" sx={{width: '100%', paddingTop: 2, marginTop: 2, backgroundColor: 'purple', fontWeight: "Bold"}}>{button}</Button>
+            <Button variant="contained" size="large" sx={{width: '100%', paddingTop: 2, marginTop: 2, backgroundColor: 'orange'}}>{button}</Button>
           </CardActions>
         </Card>
         </FadeIn>
