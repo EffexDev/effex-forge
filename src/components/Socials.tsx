500c048f8d9d601a1b2497621474f2817662dcc1
EffexDev <effexdev@gmail.com>
2025-02-19 00:08:52 +0800

    Fixed sec issue with socials links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,13 +1,15 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
-import XIcon from '@mui/icons-material/X';
+// import XIcon from '@mui/icons-material/X';
+import { Facebook } from '@mui/icons-material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Socials({ flexDirection = "row"}) {
     return(
         <div className={`flex flex-${flexDirection} w-30 justify-evenly`}>
             <a href="https://github.com/EffexDev"><GitHubIcon /></a>
-            <a href="https://x.com/EffexForge" ><XIcon /></a>
-            <a href="https://www.instagram.com/thesmoothdescent/?next=%2F" ><InstagramIcon /></a>
+            {/* <a href="https://x.com/EffexForge" ><XIcon /></a> */}
+            <a href="https://www.facebook.com/profile.php?id=61572436185675" ><Facebook /></a>
+            <a href="https://www.instagram.com/effexforge/" ><InstagramIcon /></a>
         </div>
     )
 }
